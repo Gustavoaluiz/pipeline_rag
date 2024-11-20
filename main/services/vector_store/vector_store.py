744c7769032d3ae6047f8801abc218e77f473352
gustavoaluiz <gustavoaluizbp@gmail.com>
2024-11-19 22:16:27 -0300

    feat(vector_store): finaliza a vector store

--- main/services/vector_store/vector_store.py
+++ main/services/vector_store/vector_store.py
@@ -9,15 +9,18 @@ from pandas import DataFrame
 import numpy as np
 from typing import Tuple
 from numpy import ndarray
-from main.services.encoder.embedding import Embedding
+from main.services.encoder.embedding_port import EmbeddingPort
 from main.services.chunker.chunker_hashes import ChunksHashes
+from typing import List
+# import os
+# os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
 
 class VectorStoreFaissAdaper:
 
     def __init__(
             self, 
-            encoder: Embedding, 
-            embedding_dim: int,
+            encoder: EmbeddingPort, 
             index_path: str = None,
             index_type: str = 'flat', 
     ):
@@ -34,7 +37,7 @@ class VectorStoreFaissAdaper:
         if os.path.exists(self.index_file):
             self.index = self._read_index()
         else:
-            self.index = self._create_index(embedding_dim)
+            self.index = self._create_index(self.encoder.embedding_dim)
 
     def _create_index(self, embedding_dim: int):
         os.makedirs(self.index_path, exist_ok=True)
@@ -58,21 +61,20 @@ class VectorStoreFaissAdaper:
             print('No new chunks to add.')
             return
         
-        embeddings = self.encoder.embed_documents(chunks)
-        # Garantir que os dados sejam armazenados de forma contígua
-        # na memória
+        embeddings = self.encoder.embed_documents(chunks, task='RETRIEVAL_DOCUMENT')
+        # Garantir que os dados sejam armazenados de forma contígua na memória
         embeddings = np.ascontiguousarray(embeddings)
         faiss.normalize_L2(embeddings)
-
-        if not self.index.is_trained:
-            self.index.train(embeddings)
 
         self.index.add(embeddings)
         self._save_index()
 
     def search(self, query_text: str, k: int = 5) -> Tuple[ndarray, ndarray]:
-        query_embedding = self.encoder.embed_query(query_text)
+        query_embedding = self.encoder.embed_query(query_text, task='RETRIEVAL_QUERY')
         query_embedding = query_embedding.reshape(1, -1)
+        query_embedding = np.ascontiguousarray(query_embedding)
+        faiss.normalize_L2(query_embedding)
+
         distances, indexes = self.index.search(query_embedding, k)
 
         return distances, indexes
